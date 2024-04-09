a7b567eb6b9db44c7a771aa388577f79bd5a5f4d
unknown <huaiyugm@126.com>
2024-04-09 23:36:48 +0800

    init estimator

---
+++ Estimator.py
@@ -0,0 +1,57 @@
+class Estimator():
+    """
+    Initialize the Estimator with the given parameters. Default precision is 16-bit.
+    a number of attention heads 
+    p pipeline parallel size 
+    b microbatch size
+    s sequence length 
+    h hidden dimension size
+    t tensor parallel size 
+    L number of transformer layers 
+    v vocabulary size
+    """
+    def __init__(self, model_params=7e9, optimizer='adam', a=32, p=1, b=1, s=512, h=4096, t=1, v=32000, L=32):
+        self.model_params=model_params
+        self.optimizer=optimizer
+        self.a=a
+        self.p=p
+        self.b=b
+        self.s=s
+        self.h=h
+        self.t=t
+        self.v=v 
+        self.L=L
+    def estimator_gpu_memory(self, **params):
+        if 'b' in params:
+            b=params['b']
+        else:
+            b=self.b
+        if 's' in params:
+            s=params['s']
+        else:
+            s=self.s
+        
+        if 'stage' in params:
+            stage=params['stage']
+        else:
+            stage=0
+        if 'gpu_num' in params:
+            gpu_num=params['gpu_num']
+        else:
+            gpu_num=1
+
+        if stage==1:
+            model_memory=(2+2+12/gpu_num)*self.model_params
+        elif stage==2:
+            model_memory=(2+(2+12)/gpu_num)*self.model_params
+        elif stage==3:
+            model_memory=((2+2+12)/gpu_num)*self.model_params
+        else:
+            model_memory=(2+2+12)*self.model_params
+        activation_memory=s*b*self.h*(34+5*self.a*s/self.h+4*self.v/(self.L*self.h))*self.L/self.t
+        buffer_memory=2*self.model_params
+        total_memory=model_memory+activation_memory+buffer_memory
+        gb=1024**3
+        return total_memory/gb, model_memory/gb, activation_memory/gb, buffer_memory/gb
+
+
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # LLMGPUMemEstimator
 The GPU RAM Estimator provides a simple tool for estimating GPU memory usage during training and inference.
+## Estimator
+### Usage
+'''python
+e=Estimator()
+
+# single gpu, stage 0, memory_per_gpu(GB): 120.7826795578003, model_memory: 104.3081283569336, activation_memory: 3.43603515625, buffer_memory: 13.0385160446167
+print("single gpu, stage 0, memory_per_gpu(GB): {}, model_memory: {}, activation_memory: {}, buffer_memory: {}".format(*e.estimator_gpu_memory()))
+
+# 4 gpu, stage 3, memory_per_gpu(GB): 42.5515832901001, model_memory: 26.0770320892334, activation_memory: 3.43603515625, buffer_memory: 13.0385160446167
+print("4 gpu, stage 3, memory_per_gpu(GB): {}, model_memory: {}, activation_memory: {}, buffer_memory: {}".format(*e.estimator_gpu_memory(stage=3, gpu_num=4)))
+
+# 4 gpu, stage 3, seq_len 2048, memory_per_gpu(GB): 67.8596887588501, model_memory: 26.0770320892334, activation_memory: 28.744140625, buffer_memory: 13.0385160446167
+print("4 gpu, stage 3, seq_len 2048, memory_per_gpu(GB): {}, model_memory: {}, activation_memory: {}, buffer_memory: {}".format(*e.estimator_gpu_memory(stage=3, gpu_num=4, s=2048)))
+'''
+
+## Reference
+[1] Reducing Activation Recomputation in Large Transformer Models
+[2] ZeRO: Memory Optimizations Toward Training Trillion Parameter Models
